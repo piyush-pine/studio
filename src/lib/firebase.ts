93d4b106c0350979485004d7912ae4f974c8bca4
Andy Verma <vermaandy613@gmail.com>
2025-08-22 18:55:37 +0000

    bro still even i try to sign up it throws error of firebse: Error(auth/c

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  "projectId": "dharma-treasury",
-  "appId": "1:503946405167:web:923c763958781b274a2dda",
-  "storageBucket": "dharma-treasury.firebasestorage.app",
-  "apiKey": "AIzaSyDdlzQD_TsZB55y5bP8SI9jAOpFJ5BegHc",
-  "authDomain": "dharma-treasury.firebaseapp.com",
-  "messagingSenderId": "503946405167",
+  apiKey: "AIzaSyDdlzQD_TsZB55y5bP8SI9jAOpFJ5BegHc",
+  authDomain: "dharma-treasury.firebaseapp.com",
+  projectId: "dharma-treasury",
+  storageBucket: "dharma-treasury.appspot.com",
+  messagingSenderId: "503946405167",
+  appId: "1:503946405167:web:923c763958781b274a2dda",
 };
 
 // Initialize Firebase
